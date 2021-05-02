be195cb943f031b851b689fe23451d17f932e3b0
Wellington Silva <well.silvaa97@gmail.com>
2021-05-02 00:59:06 -0300

    adicionado paginação

--- public/script.js
+++ public/script.js
@@ -6,3 +6,56 @@ for (item of menuItem) {
         item.classList.add('active')
     }
 }
+
+// Paginacao
+
+
+function paginate(selectedPage, totalPages){
+    let pages= [],
+        oldPage
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pageBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if(firstAndLastPage || pageBeforeSelectedPage && pagesAfterSelectedPage){
+            if(oldPage && currentPage - oldPage > 2){
+                pages.push("...")
+            }
+
+            if(oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+            
+            oldPage = currentPage
+        }
+    }
+
+    return pages
+}
+
+const pagination = document.querySelector(".pagination")
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+const filter = pagination.dataset.filter
+const pages = paginate(page, total)
+
+let elements = ""
+
+for (let page of pages) {
+    if(String(page).includes("...")){
+        elements += `<span>${page}</span>`
+    } else {
+        if(filter){
+            elements += `<a href="?page=${page}&filter=${filter}}">${page}</a>`
+        } else {
+            elements += `<a href="?page=${page}">${page}</a>`
+        }
+    }
+}
+
+pagination.innerHTML = elements
--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -4,19 +4,28 @@ const { age, graduation, date } = require('../../lib/utils')
 const controller = {
     index: (req, res) => {
 
-        const { filter } = req.query
+        let { filter, page, limit } = req.query
 
-        if(filter) {
-            Teacher.findBy(filter, (teachers) => {
-                return res.render('teachers/index', { teachers, filter })
-            })
-        } else {
-            Teacher.all((teachers) => {
-                return res.render('teachers/index', { teachers })
-            })
+        page = page || 1
+        limit = limit || 2
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(teachers){
+
+                const pagination = {
+                    total: Math.ceil(teachers[0].total / limit),
+                    page
+                }
+                    return res.render('teachers/index', { teachers, pagination, filter })
+            }
         }
+        Teacher.paginate(params)
 
-        
 
     },
     show: (req, res) => {
